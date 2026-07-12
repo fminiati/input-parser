0f8a16b73301c3619c035b8a617769488e6703c2
fminiati <fminiati@protonmail.com>
2026-07-12 20:49:55 +0200

    Add test: backslash before comment continues on next line

---
+++ test/test_cont_before_comment.cpp
@@ -0,0 +1,26 @@
+#include <cstdlib>
+#include <fstream>
+#include <string>
+#include <vector>
+#include "FileParser.h"
+
+int main()
+{
+    const std::string path = "test/test_cont_before_comment_input";
+    {
+        std::ofstream out(path);
+        out << "data = 12.0 2 3 \\  # continue below\n";
+        out << "      7.0 0.0\n";
+    }
+
+    fm::FileParser parser(path);
+
+    std::vector<double> v(5);
+    parser.get_items(v, "data");
+
+    if (v[0] != 12.0 || v[1] != 2.0 || v[2] != 3.0 || v[3] != 7.0 || v[4] != 0.0)
+        return 1;
+
+    std::remove(path.c_str());
+    return 0;
+}
