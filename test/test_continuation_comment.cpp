d788d6917a8cfcae8b910f416f4b2ecb7b10e2fb
fminiati <fminiati@protonmail.com>
2026-07-12 20:49:51 +0200

    Add test: backslash continuation with different types

---
+++ test/test_continuation_comment.cpp
@@ -0,0 +1,26 @@
+#include <cstdlib>
+#include <fstream>
+#include <string>
+#include <vector>
+#include "FileParser.h"
+
+int main()
+{
+    const std::string path = "test/test_continuation_comment_input";
+    {
+        std::ofstream out(path);
+        out << "data = 10 20 \\\n";
+        out << "     30 40\n";
+    }
+
+    fm::FileParser parser(path);
+
+    std::vector<int> v(4);
+    parser.get_items(v, "data");
+
+    if (v[0] != 10 || v[1] != 20 || v[2] != 30 || v[3] != 40)
+        return 1;
+
+    std::remove(path.c_str());
+    return 0;
+}
