9c27990f5d33e97a66e4639cdade788326e78024
fminiati <fminiati@protonmail.com>
2026-07-12 20:49:43 +0200

    Add test: get_items with std::array

---
+++ test/test_array.cpp
@@ -0,0 +1,28 @@
+#include <cstdlib>
+#include <fstream>
+#include <string>
+#include <array>
+#include "FileParser.h"
+
+int main()
+{
+    const std::string path = "test/test_array_input";
+    {
+        std::ofstream out(path);
+        out << "data = 5 6 7 8 9\n";
+    }
+
+    fm::FileParser parser(path);
+
+    std::array<int, 5> a{};
+    parser.get_items(a, "data");
+
+    if (a[0] != 5) return 1;
+    if (a[1] != 6) return 2;
+    if (a[2] != 7) return 3;
+    if (a[3] != 8) return 4;
+    if (a[4] != 9) return 5;
+
+    std::remove(path.c_str());
+    return 0;
+}
